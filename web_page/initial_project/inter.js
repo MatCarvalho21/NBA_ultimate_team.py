c123b3392af2e59ba079fe44c9172fc2102db55c
Matheus <matheuscarvalho210404@outlook.com>
2023-11-25 17:31:57 -0300

    ajustando a troca de times de ambos os lados

--- web_page/initial_project/inter.js
+++ web_page/initial_project/inter.js
@@ -11,4 +11,12 @@ function mudar_imagem_quadra() {
     document.getElementById("imagem_quadra").src = "imagens_de_quadras/quadra_" + selecionado + ".png";
   }
 
-  
+function mudar_time(numero_escalacao){
+  var selecionado = document.getElementById("escolha_escalacao_" + numero_escalacao).value;
+
+  document.getElementById("PG" + numero_escalacao).src = "imagens_de_times/" + selecionado + "/PG_" + selecionado + ".png";
+  document.getElementById("SG" + numero_escalacao).src = "imagens_de_times/" + selecionado + "/SG_" + selecionado + ".png";
+  document.getElementById("SF" + numero_escalacao).src = "imagens_de_times/" + selecionado + "/SF_" + selecionado + ".png";
+  document.getElementById("PF" + numero_escalacao).src = "imagens_de_times/" + selecionado + "/PF_" + selecionado + ".png";
+  document.getElementById("CE" + numero_escalacao).src = "imagens_de_times/" + selecionado + "/CE_" + selecionado + ".png";
+}
