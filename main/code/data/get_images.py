20881041d6dcfb1331d874a599f20688229484d4
Matheus Carvalho <matheuscarvalho210404@outlook.com>
2024-01-16 13:07:01 -0300

    criando a função que salva as imagens dos jogadores e salvando propriamente as imagens

--- main/code/data/get_images.py
+++ main/code/data/get_images.py
@@ -1,4 +1,5 @@
 
+
 import pandas as pd
 import requests
 
@@ -22,3 +23,21 @@ def saveFlags(dataframe:pd.DataFrame, path_for_save:str="main/assets/images/flag
                 with open(f"{path_for_save}/{each_country}.png", "wb") as file:
                     file.write(response.content)
                 print(f'{each_country} pronto.')
+
+def savePlayers(dataframe:pd.DataFrame, path_for_save:str="main/assets/images/players") -> None:
+    """
+    Downloads and saves player images based on 'NBA_ID' column in the dataframe.
+
+    Args:
+    dataframe (pd.DataFrame): The dataframe containing an 'NBA_ID' column and an 'IMAGE_URL' column.
+    path_for_save (str, optional): The path where the player images will be saved. Defaults to "main/assets/images/players".
+
+    Returns:
+    None
+    """
+    for each_id, each_player_link in zip(dataframe["NBA_ID"], dataframe["IMAGE_URL"]):
+        response = requests.get(each_player_link)
+        if response.status_code == 200:
+            with open(f"{path_for_save}/{each_id}.png", "wb") as file:
+                file.write(response.content)
+            print(f'{each_id} pronto.')
