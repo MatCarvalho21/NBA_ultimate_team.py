d44d54dec4174cfed50bad67627a72bc14523922
Matheus Carvalho <matheuscarvalho210404@outlook.com>
2024-01-17 00:17:44 -0300

    finalizando a criação das mini cartasm (que não possuem texto)

---
+++ main/code/design/make_mini_cards.py
@@ -0,0 +1,63 @@
+from PIL import Image, ImageDraw
+import pandas as pd
+import numpy as np
+
+def saveMiniCard(index:int=0, path_for_save:str="main/assets/images/mini_cards") -> None:
+    """
+    Creates and saves a mini card image for a player based on their information in the players.csv file.
+
+    Args:
+    index (int, optional): Index of the player in the players.csv file. Defaults to 0.
+    path_for_save (str, optional): The path where the mini card image will be saved. Defaults to "main/assets/images/mini_cards".
+
+    Returns:
+    None
+    """
+
+    dataframe = pd.read_csv("main/assets/database/players.csv")
+    row_list = dataframe.loc[index, :].values.flatten().tolist()
+
+    im_card = Image.open("main/assets/images/cards/base_cards/mini_gold.png").convert("RGBA")
+    im_player = Image.open(f"main/assets/images/players/{row_list[1]}.png").convert("RGBA")
+    im_league = Image.open("main/assets/images/league_image.png").convert("RGBA")
+    im_flag = Image.open(f"main/assets/images/flags/{row_list[-1].lower()}.png")
+
+    card_draw = ImageDraw.Draw(im_card)
+
+    im_card = im_card.resize((1300, 1550))
+
+    # FLAGS
+    if row_list[-1] == "CH":
+        im_flag = im_flag.resize((100, 100))
+        im_card.paste(im_flag, (350, 1225))
+    else:
+        im_flag = im_flag.resize((160, 100))
+        im_card.paste(im_flag, (320, 1225))
+        
+    # LEAGUE
+    im_league = im_league.resize((200, 200))
+    league_ar = np.array(im_league)
+    league_mk = (league_ar[:, :, 3] > 0)
+    im_card.paste(im_league, (550, 1175), Image.fromarray(league_mk))
+
+    # TEAM
+    team_name = (row_list[-11].lower()).replace(" ", "_")
+    im_team = Image.open(f"main/assets/images/teams/logo_{team_name}.png").convert("RGBA")
+    if team_name == "unemployed":
+        im_team = im_team.resize((140, 140))
+        team_ar = np.array(im_team)
+        team_mk = (team_ar[:, :, 3] > 0)
+        im_card.paste(im_team, (800, 1200), Image.fromarray(team_mk))
+    else:
+        im_team = im_team.resize((180, 180))
+        team_ar = np.array(im_team)
+        team_mk = (team_ar[:, :, 3] > 0)
+        im_card.paste(im_team, (800, 1175), Image.fromarray(team_mk))
+
+    # PLAYER
+    im_player = im_player.resize((900, 700))
+    player_ar = np.array(im_player)
+    player_mk = (player_ar[:, :, 3] > 200)
+    im_card.paste(im_player, (200, 420), Image.fromarray(player_mk))
+
+    im_card.save(path_for_save + f"/{row_list[1]}.png")
