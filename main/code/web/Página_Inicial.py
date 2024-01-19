58d6aa6efd2fa01755c615ed4738de8fdd9529dd
Matheus Carvalho <matheuscarvalho210404@outlook.com>
2024-01-18 22:53:14 -0300

    avanços na criação da página principal e da página do jogador

--- main/code/web/Página_Inicial.py
+++ main/code/web/Página_Inicial.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd  
-from functions import rakingCreator, DICT_ATRIBUTOS
+from functions import rakingCreator, colorAttribute, DICT_ATRIBUTOS
 
 def main():
     """
@@ -10,24 +10,24 @@ def main():
     st.markdown("<h1 style='text-align: center;'>NBA Ultimate Team</h1>", unsafe_allow_html=True)
     st.markdown("---")
 
-    st.markdown("<h2 style='text-align: center;'>Busque um Jogador</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center;'>Player Selector</h2>", unsafe_allow_html=True)
     option = st.selectbox(label="",
                           options=list(dataframe["FULL_NAME"]),
                           index=None,
-                          placeholder="busque um jogador")
+                          placeholder="search for a player")
     st.markdown("---")
 
     if option == None:
-        st.markdown("<h2 style='text-align: center;'>Melhores Jogadores</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center;'>Best Players</h2>", unsafe_allow_html=True)
         dataframe = dataframe.sort_values("OVERALLATTRIBUTE", ascending=False).reset_index(drop=True)
         rakingCreator(dataframe)
         st.markdown("---")
 
-        st.markdown("<h2 style='text-align: center;'>Gerador de Rankings</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center;'>Ranking Generator</h2>", unsafe_allow_html=True)
         option = st.selectbox(label="",
                           options=list(DICT_ATRIBUTOS.keys()),
                           index=None,
-                          placeholder="busque um atributo")
+                          placeholder="search for an attribute")
         st.markdown("---")
         
         if option != None:
@@ -42,21 +42,153 @@ def main():
         row_list = dataframe.loc[0, :].values.flatten().tolist()
 
         st.markdown(f"<h2 style='text-align: center;'>{option}</h2>", unsafe_allow_html=True)
+
         col1, col2 = st.columns(2)
         with col1:
-            st.image(f"main/assets/images/full_cards/full_{row_list[1]}.png", width=300)
+            tab__1, tab__2 = st.tabs(["Mini Card", "Full Card"])
+            tab__1.markdown("", unsafe_allow_html=True)
+            tab__1.image(f"main/assets/images/mini_cards/{row_list[1]}.png", width=300)
+            tab__2.image(f"main/assets/images/full_cards/full_{row_list[1]}.png", width=300)
+
         with col2:
             st.markdown("", unsafe_allow_html=True)
             st.markdown("", unsafe_allow_html=True)
             st.markdown("", unsafe_allow_html=True)
-            st.markdown(f"<h5>NBA ID: {row_list[1]}</h5>", unsafe_allow_html=True)
-            st.markdown(f"<h5>EQUIPE: {row_list[-11]}</h5>", unsafe_allow_html=True)
-            st.markdown(f"<h5>DATA DE NASC.: {row_list[-4]}/{row_list[-5]}/{row_list[-6]}</h5>", unsafe_allow_html=True)
-            st.markdown(f"<h5>IDADE: {row_list[-7]}</h5>", unsafe_allow_html=True)
-            st.markdown(f"<h5>POSITION: {row_list[-9]}</h5>", unsafe_allow_html=True)
-            st.markdown(f"<h5>NACIONALIDADE: {row_list[-8]}</h5>", unsafe_allow_html=True)
-            st.markdown(f"<h5>Nº DE CAMISA: {row_list[-10]}</h5>", unsafe_allow_html=True)
-            st.markdown(f"<h5>EXPERIÊNCIA: {row_list[-3]} anos</h5>", unsafe_allow_html=True)
+            st.markdown("", unsafe_allow_html=True)
+            st.markdown("", unsafe_allow_html=True)
+            st.markdown("", unsafe_allow_html=True)
+
+            with st.container(border=True):
+                st.markdown(f"<h5>NBA ID: {row_list[1]}</h5>", unsafe_allow_html=True)
+                st.markdown(f"<h5>TEAM: {row_list[-11]}</h5>", unsafe_allow_html=True)
+                st.markdown(f"<h5>DATE OF BIRTH: {row_list[-5].title()} {row_list[-4]}, {row_list[-6]}</h5>", unsafe_allow_html=True)
+                st.markdown(f"<h5>AGE: {row_list[-7]}</h5>", unsafe_allow_html=True)
+                st.markdown(f"<h5>POSITION: {row_list[-9]}</h5>", unsafe_allow_html=True)
+                st.markdown(f"<h5>NACIONALITY: {row_list[-8]}</h5>", unsafe_allow_html=True)
+                st.markdown(f"<h5>JERSEY NUM.: {row_list[-10]}</h5>", unsafe_allow_html=True)
+                if row_list != "Rookie":
+                    st.markdown(f"<h5>EXPERIENCE: {row_list[-3]}</h5>", unsafe_allow_html=True)
+                else:
+                    st.markdown(f"<h5>EXPERIENCE: {row_list[-3]} year(s)</h5>", unsafe_allow_html=True)
+        
+        tab1, tab2 = st.tabs(["Attributes", "Chart"])
+
+        coluna_1, coluna_2, coluna_3 = tab1.columns(3)
+
+        with coluna_1:
+            with st.container(border=True):
+                st.markdown(f"<h5 style='text-align: center;'>Outside Scoring</h5>", unsafe_allow_html=True)
+                coluna_1_1, coluna_1_2 = st.columns([1, 4])
+                with coluna_1_1:
+                    colorAttribute(row_list[8])
+                    colorAttribute(row_list[9])
+                    colorAttribute(row_list[10])
+                    colorAttribute(row_list[11])
+                    colorAttribute(row_list[12])
+                    colorAttribute(row_list[13])
+                with coluna_1_2:
+                    st.markdown(f"<p>Close Shot</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Mid-Range Shot</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Three-Point Shot</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Free Throw</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Shot IQ</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Off. Consistency</p>", unsafe_allow_html=True)
+
+            with st.container(border=True):
+                st.markdown(f"<h5 style='text-align: center;'>Inside Scoring</h5>", unsafe_allow_html=True)
+                coluna_1_3, coluna_1_4 = st.columns([1, 4])
+                with coluna_1_3:
+                    colorAttribute(row_list[14])
+                    colorAttribute(row_list[15])
+                    colorAttribute(row_list[16])
+                    colorAttribute(row_list[17])
+                    colorAttribute(row_list[18])
+                    colorAttribute(row_list[19])
+                    colorAttribute(row_list[20])
+                    colorAttribute(row_list[21])
+                with coluna_1_4:
+                    st.markdown(f"<p>Layup</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Standing Dunk</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Driving Dunk</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Post Hook</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Post Fade</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Post Control</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Draw Foul</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Hands</p>", unsafe_allow_html=True)
+
+        with coluna_2:
+            with st.container(border=True):
+                st.markdown(f"<h5 style='text-align: center;'>Defending</h5>", unsafe_allow_html=True)
+                coluna_2_1, coluna_2_2 = st.columns([1, 4])
+                with coluna_2_1:
+                    colorAttribute(row_list[22])
+                    colorAttribute(row_list[23])
+                    colorAttribute(row_list[24])
+                    colorAttribute(row_list[25])
+                    colorAttribute(row_list[26])
+                    colorAttribute(row_list[27])
+                    colorAttribute(row_list[28])
+                    colorAttribute(row_list[29])
+                with coluna_2_2:
+                    st.markdown(f"<p>Interior Defense</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Perimeter Defense</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Steal</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Block</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Lateral Quickness</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Help Defense IQ</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Pass Perception</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Def. Consistency</p>", unsafe_allow_html=True)
+
+            with st.container(border=True):
+                st.markdown(f"<h5 style='text-align: center;'>Rebounding</h5>", unsafe_allow_html=True)
+                coluna_2_3, coluna_2_4 = st.columns([1, 4])
+                with coluna_2_3:
+                    colorAttribute(row_list[42])
+                    colorAttribute(row_list[43])
+                with coluna_2_4:
+                    st.markdown(f"<p>Off. Rebound</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Def. Rebound</p>", unsafe_allow_html=True)
+
+        with coluna_3:
+            with st.container(border=True):
+                st.markdown(f"<h5 style='text-align: center;'>Playmaking</h5>", unsafe_allow_html=True)
+                coluna_3_1, coluna_3_2 = st.columns([1, 4])
+                with coluna_3_1:
+                    colorAttribute(row_list[37])
+                    colorAttribute(row_list[38])
+                    colorAttribute(row_list[39])
+                    colorAttribute(row_list[40])
+                    colorAttribute(row_list[41])
+                with coluna_3_2:
+                    st.markdown(f"<p>Pass Accuracy</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Ball Handle</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Speed with Ball</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Pass IQ</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Pass Vision</p>", unsafe_allow_html=True)
+
+            with st.container(border=True):
+                st.markdown(f"<h5 style='text-align: center;'>Athleticism</h5>", unsafe_allow_html=True)
+                coluna_3_3, coluna_3_4 = st.columns([1, 4])
+                with coluna_3_3:
+                    colorAttribute(row_list[30])
+                    colorAttribute(row_list[31])
+                    colorAttribute(row_list[32])
+                    colorAttribute(row_list[33])
+                    colorAttribute(row_list[34])
+                    colorAttribute(row_list[35])
+                    colorAttribute(row_list[36])
+                with coluna_3_4:
+                    st.markdown(f"<p>Speed</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Acceleration</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Strength</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Vertical</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Stamina</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Hustle</p>", unsafe_allow_html=True)
+                    st.markdown(f"<p>Overall Durability</p>", unsafe_allow_html=True)
+
+
+        tab1.markdown("---")
+
 
 if __name__ == "__main__":
     main()
--- main/code/web/functions.py
+++ main/code/web/functions.py
@@ -4,82 +4,81 @@ import pandas as pd
 import streamlit as st
 
 DICT_ATRIBUTOS = {
-    "Arremesso Curto": "CLOSESHOT",
-    "Areemesso de Média Distância": "MIDRANGESHOT",
-    "Arremesso de 3 Pontos": "THREEPOINTSHOT",
-    "Lance Livre": "FREETHROW",
-    "Seleção de Arremesso": "SHOTIQ",
-    "Consistência Ofensiva": "OFFENSIVECONSISTENCY",
-    "Bandeja": "LAYUP",
-    "Enterrada Parada": "STANDINGDUNK",
-    "Enterrada Em Movimento": "DRIVINGDUNK",
-    "Gancho": "POSTHOOK",
-    "Controle de Garrafão": "POSTCONTROL",
-    "Provocar Faltas": "DRAWFOUL",
+    "Close Shot": "CLOSESHOT",
+    "Mid-Range Shot": "MIDRANGESHOT",
+    "Three-Point Shot": "THREEPOINTSHOT",
+    "Free Throw": "FREETHROW",
+    "Shot IQ": "SHOTIQ",
+    "Offensive Consistency": "OFFENSIVECONSISTENCY",
+    "Layup": "LAYUP",
+    "Standing Dunk": "STANDINGDUNK",
+    "Driving Dunk": "DRIVINGDUNK",
+    "Post Hook": "POSTHOOK",
+    "Post Fade": "POSTFADE",
+    "Post Control": "POSTCONTROL",
+    "Draw Foul": "DRAWFOUL",
     "Hands": "HANDS",
-    "Defesa de Garrafão": "INTERIORDEFENSE",
-    "Defesa de Perímetro": "PERIMETERDEFENSE",
-    "Roubo de Bola": "STEAL",
-    "Bloqueio": "BLOCK",
-    "Agilidade Lateral": "LATERALQUICKNESS",
-    "Inteligência Defensiva": "HELPDEFENSEIQ",
-    "Percepção de Passe": "PASSPERCEPTION",
-    "Consistência Defensiva": "DEFENSIVECONSISTENCY",
-    "Velocidade": "SPEED",
-    "Aceleração": "ACCELERATION",
-    "Força": "STRENGTH",
-    "Salto Vertical": "VERTICAL",
-    "Fôlego": "STAMINA",
-    "Esforço": "HUSTLE",
-    "Durabilidade do Overall": "OVERALLDURABILITY",
-    "Precisão de Passe": "PASSACCURACY",
-    "Controle de Bola": "BALLHANDLE",
-    "Velocidade Com a Bola": "SPEEDWITHBALL",
-    "Seleção de Passe": "PASSIQ",
-    "Visão de Passe": "PASSVISION",
-    "Rebote Ofensivo": "OFFENSIVEREBOUND",
-    "Rebote Defensivo": "DEFENSIVEREBOUND"
+    "Interior Defense": "INTERIORDEFENSE",
+    "Perimeter Defense": "PERIMETERDEFENSE",
+    "Steal": "STEAL",
+    "Block": "BLOCK",
+    "Lateral Quickness": "LATERALQUICKNESS",
+    "Help Defense IQ": "HELPDEFENSEIQ",
+    "Pass Perception": "PASSPERCEPTION",
+    "Defensive Consistency": "DEFENSIVECONSISTENCY",
+    "Speed": "SPEED",
+    "Acceleration": "ACCELERATION",
+    "Strength": "STRENGTH",
+    "Vertical": "VERTICAL",
+    "Stamina": "STAMINA",
+    "Hustle": "HUSTLE",
+    "Overall Durability": "OVERALLDURABILITY",
+    "Pass Accuracy": "PASSACCURACY",
+    "Ball Handle": "BALLHANDLE",
+    "Speed with Ball": "SPEEDWITHBALL",
+    "Pass IQ": "PASSIQ",
+    "Pass Vision": "PASSVISION",
+    "Offensive Rebound": "OFFENSIVEREBOUND",
+    "Defensive Rebound": "DEFENSIVEREBOUND"
 }
 
 def rakingCreator(dataframe:pd.DataFrame) -> None:
     """
     """
 
-    col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12 = st.columns(12)
+    col1, col2, col3, col4, col5 = st.columns(5)
     imagens_urls = list(dataframe["NBA_ID"])
     with col1:
-        for i in range(0, 36, 12):
+        for i in range(0, 15, 5):
             st.image(f"main/assets/images/full_cards/full_{imagens_urls[i]}.png")
     with col2:
-        for i in range(1, 37, 12):
+        for i in range(1, 15, 5):
             st.image(f"main/assets/images/full_cards/full_{imagens_urls[i]}.png")
     with col3:
-        for i in range(2, 38, 12):
+        for i in range(2, 15, 5):
             st.image(f"main/assets/images/full_cards/full_{imagens_urls[i]}.png")
     with col4:
-        for i in range(3, 39, 12):
+        for i in range(3, 15, 5):
             st.image(f"main/assets/images/full_cards/full_{imagens_urls[i]}.png")
     with col5:
-        for i in range(4, 40, 12):
+        for i in range(4, 15, 5):
             st.image(f"main/assets/images/full_cards/full_{imagens_urls[i]}.png")
-    with col6:
-        for i in range(5, 41, 12):
-            st.image(f"main/assets/images/full_cards/full_{imagens_urls[i]}.png")
-    with col7:
-        for i in range(6, 42, 12):
-            st.image(f"main/assets/images/full_cards/full_{imagens_urls[i]}.png")
-    with col8:
-        for i in range(7, 43, 12):
-            st.image(f"main/assets/images/full_cards/full_{imagens_urls[i]}.png")
-    with col9:
-        for i in range(8, 44, 12):
-            st.image(f"main/assets/images/full_cards/full_{imagens_urls[i]}.png")
-    with col10:
-        for i in range(9, 45, 12):
-            st.image(f"main/assets/images/full_cards/full_{imagens_urls[i]}.png")
-    with col11:
-        for i in range(10, 46, 12):
-            st.image(f"main/assets/images/full_cards/full_{imagens_urls[i]}.png")
-    with col12:
-        for i in range(11, 47, 12):
-            st.image(f"main/assets/images/full_cards/full_{imagens_urls[i]}.png")
+
+
+def colorAttribute(attribute:int) -> None:
+    """
+    """
+    if attribute >= 90:
+        st.markdown(f"<p style='color: #122273'>{attribute}</p>", unsafe_allow_html=True)
+    elif attribute >= 80:
+        st.markdown(f"<p style='color: #008000'>{attribute}</p>", unsafe_allow_html=True)
+    elif attribute >= 70:
+        st.markdown(f"<p style='color: #00FF00'>{attribute}</p>", unsafe_allow_html=True)
+    elif attribute >= 60:
+        st.markdown(f"<p style='color: #EDFA00'>{attribute}</p>", unsafe_allow_html=True)
+    elif attribute >= 50:
+        st.markdown(f"<p style='color: #FFFF00'>{attribute}</p>", unsafe_allow_html=True)
+    elif attribute >= 40:
+        st.markdown(f"<p style='color: orange'>{attribute}</p>", unsafe_allow_html=True)
+    else:
+        st.markdown(f"<p style='color: red'>{attribute}</p>", unsafe_allow_html=True)
